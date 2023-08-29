b323150a37198b2fef3c0ca0b932f0eff9d8a0d7
Hitesh-Attri <hiteshattri71@gmail.com>
2023-08-29 11:11:50 +0530

    trying fetch

--- app/page.js
+++ app/page.js
@@ -13,16 +13,29 @@ export default function Home() {
   }, []);
 
   const getDataFunc = async () => {
-    axios
-      .get("/api/get-data")
-      .then((res) => {
-        console.log(res.data);
-        setData(res.data.list);
-      })
-      .catch((err) => {
-        console.log(err);
-        alert("something went wrong");
+    // axios
+    //   .get("/api/get-data")
+    //   .then((res) => {
+    //     console.log(res.data);
+    //     setData(res.data.list);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     alert("something went wrong");
+    //   });
+
+    try {
+      let res = await fetch("/api/get-data", {
+        cache: "no-store",
       });
+
+      res = await res.json();
+      console.log(res);
+      // setData(res.list);
+    } catch (error) {
+      console.log(error);
+      alert("something went wrong");
+    }
   };
   const postFunc = async () => {
     axios
